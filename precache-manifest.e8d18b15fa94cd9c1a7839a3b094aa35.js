177f136c96965d9396f8c652bfc63faf1c972e97
aniket2 <aniketg2898@gmail.com>
2020-04-15 23:52:28 +0530

    Updates

---
+++ precache-manifest.e8d18b15fa94cd9c1a7839a3b094aa35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf7d9596387ce7398c870a4b73789ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c82ff58af356e05d68c",
+    "url": "/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "82c687d696030ea1ea75",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1c82ff58af356e05d68c",
+    "url": "/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82c687d696030ea1ea75",
+    "url": "/static/js/main.65b5a2a2.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  }
+]);
